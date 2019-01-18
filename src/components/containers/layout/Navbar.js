8e5a5a836da58f6f061bf837fd72a651bc7e5098
unknown <mayanmilis16@gmail.com>
2019-01-18 15:13:46 +0200

    added sidebar

--- src/components/containers/layout/Navbar.js
+++ src/components/containers/layout/Navbar.js
@@ -28,18 +28,27 @@ class Navbar extends Component{
                             <li><a href='http://localhost:3000/'>Flower TV</a></li>
                             <li><a>Sign-Up</a></li>
                                 <div className='Burger' onClick={this.handleSidebar}>
-                                
-                                <div></div>
-                                <div></div>
-                                <div></div>
+                                    <div></div>
+                                    <div></div>
+                                    <div></div>
                                 </div>  
                             </ul>
                         </nav>
                         </div>
-                    <div className='SideBar' style={{right: sidebarPosition}}>   
-    
+                    <div className='SideBar' style={{right: sidebarPosition}}>  
+                        <ul>    
+                            <li><a>Home</a></li>
+                            <li><a>Library</a></li>
+                            <li><a>Watch Anywhere</a></li>
+                            <li><a>Choose Your Plan</a></li>
+                        </ul>
+                        <div className='SideBarBtn'>
+                            <ul>    
+                                <button>Join</button>
+                                <button>Free Month Trial</button>
+                            </ul>   
+                        </div>
                      </div>
-    
             </div>
     
     
