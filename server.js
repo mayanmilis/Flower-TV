363ae8a68194d1947aed88d4124f0bfd8ecea3d1
mayanmilis <mayanmilis16@gmail.com>
2019-02-21 15:05:47 +0200

    final draft

--- server.js
+++ server.js
@@ -30,6 +30,16 @@ app.use('/api/shows', shows)
 app.use('/api/admin', admin)
 app.use('/api/mail', mail)
 
+//Serve static assers if in production
+if(process.env.NODE_ENV === 'production'){  
+    //Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req,res) =>{  
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
     const port = process.env.PORT || 5000;
 
     app.listen(port, () => console.log(`Server started on port ${port}`));
